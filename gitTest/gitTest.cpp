fd6cd805e143d397054da61c5fe1b9a07dc3b12d
XF <365057253@qq.com>
2020-01-13 17:32:15 +0800

    111

--- gitTest/gitTest.cpp
+++ gitTest/gitTest.cpp
@@ -6,7 +6,8 @@ int main()
 {
 	std::cout << "ideal thread num ==" << std::thread::hardware_concurrency() << std::endl;
 
-	std::cout << "operate IVVVVVV" << std::endl;
+	std::cout << "17£º31" << std::endl;
+
 }
 
 
