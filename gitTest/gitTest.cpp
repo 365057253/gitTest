6fc80c5bf4722db0e1972e128fd43c9103476db8
XF <365057253@qq.com>
2020-01-13 17:23:10 +0800

    std::cout << "operate IVVVVVV" << std::endl;

--- gitTest/gitTest.cpp
+++ gitTest/gitTest.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	std::cout << "ideal thread num ==" << std::thread::hardware_concurrency() << std::endl;
 
-	std::cout << "operate IV" << std::endl;
+	std::cout << "operate IVVVVVV" << std::endl;
 }
 
 
